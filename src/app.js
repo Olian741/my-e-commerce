8214f7c8f45cbf20c507282d66ebe0ae5f322be5
Olian741 <158222992+Olian741@users.noreply.github.com>
2024-09-19 21:26:33 +0200

    Fixed the bug in the slider container and added some Javascript

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
-const wrapper = document.querySelector(".sliderWrapper")
+document.addEventListener("DOMContentLoaded", function() {
+  const sliderWrapper = document.querySelector(".slider");
+  const leftBtn = document.querySelector(".slider-btn.left");
+  const rightBtn = document.querySelector(".slider-btn.right");
 
+  leftBtn.addEventListener("click", () => {
+    sliderWrapper.scrollBy({ left: -300, behavior: "smooth" });
+  });
+
+  rightBtn.addEventListener("click", () => {
+    sliderWrapper.scrollBy({ left: 300, behavior: "smooth" });
+  });
+});
