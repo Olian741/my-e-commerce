d9f62b733d9d768a1332a6713cc4b7c3d0647b82
Olian741 <158222992+Olian741@users.noreply.github.com>
2024-09-23 16:39:46 +0200

    More code for the slider

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const sliderWrapper = document.querySelector(".slider");
+document.addEventListener("DOMContentLoaded", function () {
+  const sliderWrapper = document.querySelector(".sliderWrapper");
   const leftBtn = document.querySelector(".slider-btn.left");
   const rightBtn = document.querySelector(".slider-btn.right");
 
@@ -9,5 +9,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
   rightBtn.addEventListener("click", () => {
     sliderWrapper.scrollBy({ left: 300, behavior: "smooth" });
+  });
+
+  const secondSliderWrapper = document.querySelector(".second-slider-wrapper");
+  const secondLeftBtn = document.querySelector(".second-slider-btn.left");
+  const secondRightBtn = document.querySelector(".second-slider-btn.right");
+
+  secondLeftBtn.addEventListener("click", () => {
+    secondSliderWrapper.scrollBy({ left: -300, behavior: "smooth" });
+  });
+
+  secondRightBtn.addEventListener("click", () => {
+    secondSliderWrapper.scrollBy({ left: 300, behavior: "smooth" });
   });
 });
