95e7ccef24a219ba7edfd6ba6c959a1d730b748e
Olian741 <158222992+Olian741@users.noreply.github.com>
2024-09-24 22:54:23 +0200

    Cleaned up my code and changed some images

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const sliderWrapper = document.querySelector(".sliderWrapper");
+  const sliderWrapper = document.querySelector(".slider");
   const leftBtn = document.querySelector(".slider-btn.left");
   const rightBtn = document.querySelector(".slider-btn.right");
 
   leftBtn.addEventListener("click", () => {
     sliderWrapper.scrollBy({ left: -300, behavior: "smooth" });
   });
-
   rightBtn.addEventListener("click", () => {
     sliderWrapper.scrollBy({ left: 300, behavior: "smooth" });
   });
@@ -14,12 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const secondSliderWrapper = document.querySelector(".second-slider-wrapper");
   const secondLeftBtn = document.querySelector(".second-slider-btn.left");
   const secondRightBtn = document.querySelector(".second-slider-btn.right");
-
+   
   secondLeftBtn.addEventListener("click", () => {
     secondSliderWrapper.scrollBy({ left: -300, behavior: "smooth" });
   });
-
-  secondRightBtn.addEventListener("click", () => {
+   secondRightBtn.addEventListener("click", () => {
     secondSliderWrapper.scrollBy({ left: 300, behavior: "smooth" });
   });
 });
