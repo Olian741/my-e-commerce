eea12dff5ee5ca0bd2270db8b54e2c3e26b393df
Olian741 <158222992+Olian741@users.noreply.github.com>
2024-10-03 15:23:25 +0200

    Javascript for the third slider

--- src/app.js
+++ src/app.js
@@ -12,16 +12,26 @@ document.addEventListener("DOMContentLoaded", function () {
   const secondSliderWrapper = document.querySelector(".second-slider-wrapper");
   const secondLeftBtn = document.querySelector(".second-slider-btn.left");
   const secondRightBtn = document.querySelector(".second-slider-btn.right");
-   
+
   secondLeftBtn.addEventListener("click", () => {
     secondSliderWrapper.scrollBy({ left: -300, behavior: "smooth" });
   });
-   secondRightBtn.addEventListener("click", () => {
+  secondRightBtn.addEventListener("click", () => {
     secondSliderWrapper.scrollBy({ left: 300, behavior: "smooth" });
   });
+
+  const thirdSliderWrapper = document.querySelector(".third-slider-wrapper");
+  const thirdLeftBtn = document.querySelector(".third-slider-btn.left");
+  const thirdRightBtn = document.querySelector(".third-slider-btn.right");
+
+  thirdLeftBtn.addEventListener("click", () => {
+    thirdSliderWrapper.scrollBy({ left: -300, behavior: "smooth" });
+  });
+  thirdRightBtn.addEventListener("click", () => {
+    thirdSliderWrapper.scrollBy({ left: 300, behavior: "smooth" });
+  });
 });
+
 function redirect() {
   window.location.href = "confirmation.html";
-  
 }
-
